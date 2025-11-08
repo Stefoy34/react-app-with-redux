f8614c0d0ec28635aa0814c9e27743e82f453990
stefanussebayang <stefanussebayang34@gmail.com>
2025-11-08 12:13:47 +0700

    fix(ci): konfigurasikan jsdom untuk hindari error canvas

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
   test: {
     globals: true, 
     environment: 'jsdom', 
-    setupFiles: './src/setupTests.js', 
+    setupFiles: './src/setupTests.js',
+    environmentOptions: {
+      jsdom: {
+        resources: 'usable',
+      },
+    }, 
   },
 });
